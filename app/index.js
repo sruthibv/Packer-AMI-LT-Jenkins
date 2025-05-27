b281646a0561e94cb1087a4037392c3f68cd32a7
sruthibv <bsruthiuk@gmail.com>
2025-05-27 08:06:38 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js app running on custom AMI! by using packer workshop by veera<br>Another excellent workshop by Veera Don');
+  res.send('Hello from Node.js app running on custom AMI! by using packer workshop by veera<br>Another excellent workshop by Veera Don Sir');
 });
 
 app.listen(port, () => {
